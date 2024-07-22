d61657effd27b76e8f087b98288fb849b27f961f
Mikhail <podbereznikov34@gmail.com>
2024-07-23 01:24:09 +0300

    build html+scss template

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { NewsComponent } from './news/news.component';
+import { StoreComponent } from './store/store.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { MainPageComponent } from './main-page/main-page.component';
+import { HowToPlayComponent } from './how-to-play/how-to-play.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'news', component: NewsComponent },
+    { path: 'store', component: StoreComponent },
+    { path: 'dashboard', component: DashboardComponent },
+    { path: '', component: MainPageComponent},
+    { path: 'how-to-play', component: HowToPlayComponent}
+
+];
